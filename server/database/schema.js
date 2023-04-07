2f3e8d454e1f882b5c7b1d4f751239e8f39df8a4
riyaagar96 <pnascimento@uchicago.edu>
2023-04-06 17:16:00 -0700

    initial schema

---
+++ server/database/schema.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const plantSchema = new mongoose.Schema({
+  species: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  wateringFrequency: {
+    type: Number,
+    required: true
+  },
+  lastWatered: {
+    type: Date,
+    required: true
+  },
+  notes: {
+    type: String
+  },
+  fertilizerFrequency: {
+    type: Number
+  },
+  lastFertilized: {
+    type: Date
+  },
+  potSize: {
+    type: Number
+  },
+  pottedOn: {
+    type: Date
+  },
+  soilMix: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('Plant', plantSchema);
