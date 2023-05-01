204d145ba9b1ba33a401198996ab3cfb55ed3809
Paulo Nascimento <pnascimento@uchicago.edu>
2023-05-01 16:14:00 -0700

    cleaned up plantController & plantRoutes

--- server/controllers/plantController.js
+++ server/controllers/plantController.js
@@ -1,5 +1,7 @@
 const { Plant } = require('../models/plantsModel');
 
+// ========= GET ==========
+
 exports.getPlants = async (req, res) => {
     try {
       const plants = await Plant.find();
@@ -10,7 +12,7 @@ exports.getPlants = async (req, res) => {
     }
   };
 
-  exports.getPlantById = async (req, res) => {
+exports.getPlantById = async (req, res) => {
     try {
       const plant = await Plant.findById(req.params.id);
       if (!plant) {
@@ -24,4 +26,50 @@ exports.getPlants = async (req, res) => {
       }
       res.status(500).send('Server Error');
     }
-  };
+};
+
+// exports.getPlantsByUserId = async (req, res) => {
+
+    // TODO
+
+// }
+
+// ========= POST ==========
+
+exports.addPlant = async (req, res) => {
+  try {
+    const plant = new Plant(req.body);
+    await plant.save();
+    res.status(201).json(plant);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+// ========= PUT ==========
+
+exports.updatePlantById = async (req, res) => {
+  try {
+    const plant = await Plant.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!plant) {
+      return res.status(404).send();
+    }
+    res.json(plant);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+}
+
+// ========= DELETE ==========
+
+exports.deletePlant = async (req, res) => {
+  try {
+    const plant = await Plant.findByIdAndDelete(req.params.id);
+    if (!plant) {
+      return res.status(404).send();
+    }
+    res.json(plant);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+}
--- server/routes/plantRoutes.js
+++ server/routes/plantRoutes.js
@@ -1,7 +1,6 @@
 // routes/plants.js
 
 const express = require('express');
-const { Plant } = require('../models/plantsModel');
 const plantController = require('../controllers/plantController');
 
 const router = express.Router();
@@ -12,43 +11,13 @@ router.get('/', plantController.getPlants);
 // GET /api/plants/:id
 router.get('/:id', plantController.getPlantById);
 
-// TODO: move the below POST, PUT, DELETE routes to plantController.js
-
 // POST /api/plants
-router.post('/', async (req, res) => {
-  try {
-    const plant = new Plant(req.body);
-    await plant.save();
-    res.status(201).json(plant);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+router.post('/', plantController.addPlant);
 
 // PUT /api/plants/:id
-router.put('/:id', async (req, res) => {
-  try {
-    const plant = await Plant.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!plant) {
-      return res.status(404).send();
-    }
-    res.json(plant);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+router.put('/:id', plantController.updatePlantById);
 
 // DELETE /api/plants/:id
-router.delete('/:id', async (req, res) => {
-  try {
-    const plant = await Plant.findByIdAndDelete(req.params.id);
-    if (!plant) {
-      return res.status(404).send();
-    }
-    res.json(plant);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+router.delete('/:id', plantController.deletePlant);
 
 module.exports = router;
